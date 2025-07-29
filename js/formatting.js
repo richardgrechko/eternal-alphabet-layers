253a05dc08ef311840de68a73ee53b67b9f65341
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-07-29 23:46:27 +0500

    Update formatting.js

--- js/formatting.js
+++ js/formatting.js
@@ -10,17 +10,17 @@ function fullFormat(options) {
 		num: options.num || new Decimal("1"),
 		precision: options.precision || 3,
 	}
-	if (num.gte("10^^10")) {
+	if (options.num.gte("10^^10")) {
 		return new Decimal(10).pow(options.num.slog().sub(options.num.slog().floor())) + "F" + options.num.slog().floor();
-	} else if (num.gte("ee10")) {
+	} else if (options.num.gte("ee10")) {
 		return "e" + fullFormat({num: options.num.log10(), precision: options.precision});
-	} else if (num.gte("e10")) {
+	} else if (options.num.gte("e10")) {
 		return fullFormat({num: new Decimal(10).pow(options.num.log10().sub(options.num.log10().floor())), precision: options.precision}) + "e" + options.num.log10().floor();
-	} else if (num.gte("1000000")) {
-		return commaFormat(options.num);
-	} else if (num.gte("1000")) {
+	} else if (options.num.gte("1000000")) {
+		return commaFormat(options.num.floor());
+	} else if (options.num.gte("1000")) {
 		return commaFormat(Number(options.num).toFixed(options.precision));
-	} else if (num.gte("0")) {
+	} else if (options.num.gte("0")) {
 		return Number(options.num).toFixed(options.precision);
 	};
 }
