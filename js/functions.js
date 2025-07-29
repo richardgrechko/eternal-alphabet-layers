a5493a41b181fb09051125f1f4afcdc665c4e1be
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-07-29 22:13:36 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -22,8 +22,8 @@ var functions = {
 	},
 	load() {
 		function loadValue(value,alternate) {
-			console.log((value !== null || value !== undefined || !isNaN(value) || value.toString() !== "NaN") ? value : alternate)
-			return (value !== null || value  !== undefined || !isNaN(value) || value.toString() !== "NaN") ? value : alternate;
+			console.log((value !== undefined) ? value : alternate)
+			return (value  !== undefined) ? value : alternate;
 		};
 		let item = localStorage.getItem("EternalAlphabetLayersSave");
 		if (item != null) {
