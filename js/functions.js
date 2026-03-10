2cc5e00a6d25f81e736053ca3c33b673074f56ac
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-03-10 18:09:31 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -74,7 +74,7 @@ var functions = {
 	},
 	update() {
 		data.tetration = data.tetration || 0;
-		data.tetration += ((data.tetration >= 1) ? .0001 : .001) * data.eternities.add(1).sqrt();
+		data.tetration += ((data.tetration >= 1) ? .0001/(data.tetration**0.8) : .001) * data.eternities.add(1).sqrt();
 		data.tetration *= 1 + (.0001 * Math.log10(data.tetration) * data.eternities.add(1).sqrt());
 		if (data.tetration == Infinity) {
 			data.eternities = data.eternities.add(1);
@@ -82,14 +82,21 @@ var functions = {
 		}
 		data.mode = document.getElementById("layerModeOption").value;
 		data.totalA = new Decimal("10").tetrate(data.tetration || 0);
+		let Gv = data.totalA.log(Number.MAX_VALUE).log10().add(1).tetrate(0.75)
+		let hy = Gv.div(data.totalA.slog().div(new Decimal(Number.MAX_VALUE).slog()).pow(4)).root(2)
+		let html = document.getElementsByTagName("html")[0]
 		data.number = data.totalA.gte(Number.MAX_VALUE)
-			? new Decimal(Number.MAX_VALUE).pow(data.totalA.log(Number.MAX_VALUE).log10().pow(0.75).sub(data.totalA.log(Number.MAX_VALUE).log10().pow(0.75).floor()).pow(3))
+			? new Decimal(Number.MAX_VALUE).pow(hy.sub(hy.floor()))
 			: data.totalA;
-		data.layer = data.totalA.gte(Number.MAX_VALUE) ? data.totalA.log(Number.MAX_VALUE).log10().pow(0.75).floor().add(1) : data.totalA.log(Number.MAX_VALUE).floor().add(1);
+		data.layer = data.totalA.gte(Number.MAX_VALUE) ? hy.floor().add(1) : data.totalA.log(Number.MAX_VALUE).floor().add(1);
 		document.getElementById("layers").innerHTML = data.layerHTML = data.layer.gte("10^^10") ? `<span style="color: hsl(${data.layer.slog().log10().mul(180)} 100 ${50+(Math.sin(data.layer.slog().log10())*15)+15}); text-shadow: currentcolor 0 0 0.6em, currentcolor 0 0 0.4em, currentcolor 0 0 0.2em;">${functions.convertToLayer(data.layer,data.mode)}</span>` : 
 				data.layer.gte("ee10") ? `<span style="color: hsl(${data.layer.slog().mul(180)} 100 ${55+(Math.sin(data.layer.slog())*10)+10}); text-shadow: currentcolor 0 0 0.6em, currentcolor 0 0 0.4em, currentcolor 0 0 0.2em;">${functions.convertToLayer(data.layer,data.mode)}</span>` : 
 				data.layer.gte("1e6") ? `<span style="color: hsl(${data.layer.log10().log10().mul(180)} 100 ${60+(Math.sin(data.layer.log10().log10())*5)+5}); text-shadow: currentcolor 0 0 0.6em, currentcolor 0 0 0.4em, currentcolor 0 0 0.2em;">${functions.convertToLayer(data.layer,data.mode)}</span>` : 
 				`${fullFormat({num: data.number})}<span style="color: hsl(${data.layer.log10().mul(180)} ${data.layer.gte("10") ? 100 : data.layer.mul(10)} 75); text-shadow: currentcolor 0 0 ${data.layer.gte("100") ? 0.6 : data.layer.sqrt().div(20).mul(1.2)}em, currentcolor 0 0 ${data.layer.gte("100") ? 0.4 : data.layer.sqrt().div(20).mul(0.8)}em, currentcolor 0 0 ${data.layer.gte("100") ? 0.2 : data.layer.sqrt().div(20).mul(0.4)}em;">${functions.convertToLayer(data.layer,data.mode)}</span>`;
+		document.getElementsByClassName("layer-background")[0].style.background = data.layer.gte("10^^10") ? `linear-gradient(to bottom, hsl(${data.layer.slog().log10().mul(180)} 100 ${(50+(Math.sin(data.layer.slog().log10())*15)+15)/4}), #000)` : 
+				data.layer.gte("ee10") ? `linear-gradient(to bottom, hsl(${data.layer.slog().mul(180)} 100 ${(55+(Math.sin(data.layer.slog())*10)+10)/4}), #000)` : 
+				data.layer.gte("1e6") ? `linear-gradient(to bottom, hsl(${data.layer.log10().log10().mul(180)} 100 ${(60+(Math.sin(data.layer.log10().log10())*5)+5)/4}), #000)` : 
+				`linear-gradient(to bottom, hsl(${data.layer.log10().mul(180)} ${data.layer.gte("10") ? 100 : data.layer.mul(10)} 18), #000)`;
 		document.getElementById("totalA").innerHTML = data.layer.gte("2") ? `That is also ${fullFormat({num: data.totalA})}<span style="color: hsl(0 10 75);">a</span>` : "";
 		document.getElementById("currentLayer").innerHTML = `You're currently in Layer ${fullFormat({num: data.layer, precision: 0})}`;
 		document.getElementById("eternities").innerHTML = `You have <span style="font-weight: 700;">${fullFormat({num: data.eternities, precision: 0})}</span> eternities.`;
